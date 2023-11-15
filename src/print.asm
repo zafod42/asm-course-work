59718a6af8949e36d5b3d3520add8a5bea33b5a8
Pavel Kalabin <kalabin.pp@edu.spbstu.ru>
2023-11-15 17:16:06 +0300

    refactored print module

--- src/print.asm
+++ src/print.asm
@@ -1,10 +1,4 @@
-	get_russian_str macro	; puts string in russian to buffer at DS:DX
-		pusha
-		get_str
 	
-		popa
-	endm
-
 	putchar macro		; puts char from dl
 		push ax
 		push dx
@@ -17,28 +11,29 @@
 	endm
 
 	newline macro	; just prints \n
-		pusha
+		push ax
+		push dx
+
 		mov 	ah, 2
 		mov	dl, 13
 		int 21h
 		mov 	dl, 10
 		int 21h
 
-		popa
+		pop dx
+		pop ax
 	endm
 
 	println macro	; prints string from DX and puts new line after it
-		pusha
 		print_str
 		newline
-		popa
 	endm
 
 	print_str macro		; prints string from DX
-		pusha
+		push	ax
 		mov 	ah, 9	; 
 		int 	21h
-		popa
+		pop 	ax
 	endm
 
 	get_str macro		; gets string
